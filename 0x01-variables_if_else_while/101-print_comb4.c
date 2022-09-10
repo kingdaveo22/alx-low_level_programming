3d4fe8bfb79c98dff134fb6911c02c0d608d2a77
kingdaveo22 <kingdaveo22@gmail.com>
2022-09-10 02:43:05 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,23 +8,20 @@
 */
 int main(void)
 {
-int n, m, l
+	int digit1, digit2, digit3;
+for (digit1 = 0; digit1 < 8; digit1++)
+{
+	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+{
+	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
+	putchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
+	putchar((digit3 % 10) + '0');
 
-	for (n = 48; n < 58; n++)
-{
-	for (m = 49; m < 58; m++)
-{
-	for (l = 50; l < 58; l++)
-{
-	if (l > m && m > n)
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 
-{
-	putchar(n);
-	putchar(m);
-	putchar(l);
-
-	if (n != 55 || m != 56)
-{
+	continue;
 	putchar(',');
 	putchar(' ');
 
@@ -34,13 +31,9 @@ int n, m, l
 
 }
 
-}
+putchar('\n');
 
-}
-
-	putchar('\n');
-
-	return (0);
+return (0);
 
 }
 
