2fc44d66b12e549168565691598504e1d772c84e
kingdaveo22 <kingdaveo22@gmail.com>
2022-09-13 16:16:13 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,7 @@
  * Return: times table
  * add extra space past single digit
  */
-
 void times_table(void)
-
 {
 	int r, c, d;
 
@@ -21,18 +19,17 @@ void times_table(void)
 
 	for (c = 1; c <= 9; c++)
 {
+
 	d = (r * c);
-
 	if ((d / 10) > 0)
-
 {
 	_putchar((d / 10) + '0');
-}
 
+}
 	else
+
 {
 	_putchar(' ');
-}
 
 }
 	_putchar((d % 10) + '0');
@@ -41,7 +38,6 @@ void times_table(void)
 
 {
 	_putchar(',');
-
 	_putchar(' ');
 
 }
