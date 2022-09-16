221420f5da182eeddc31cfee21cdec35b96ec482
kingdaveo22 <kingdaveo22@gmail.com>
2022-09-16 06:26:01 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,19 @@
 */
 
 void print_number(int n)
-
 {
 	unsigned int num = n;
 
 	if (n < 0)
-
 	{
-
-	putchar('-');
-
-	num = -num;
-
+		putchar('-');
+		num = -num;
 	}
-
 	if (num > 9)
-
 	{
-
-	print_number(num / 10);
-
+		print_number(num / 10);
 	}
-
 	putchar(num % 10 + '0');
 
 }
+
