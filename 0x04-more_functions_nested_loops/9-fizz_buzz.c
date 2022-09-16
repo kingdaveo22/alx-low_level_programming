a8b1d5863a37d0ba29e7ea21655b394ad6e05166
kingdaveo22 <kingdaveo22@gmail.com>
2022-09-16 03:41:49 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,58 +8,35 @@
 */
 
 int main(void)
-
 {
 	int num = 1;
 
 	while (num++ < 100)
-
 	{
 
-	if ((num % 3 == 0) && (num % 5 == 0))
-
-	{
-
-	printf("FizzBuzz ");
-
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
 	}
-
 	else if ((num % 3) == 0)
-
 	{
-
-	printf("Fizz ");
-
+		printf("Fizz ");
 	}
-
 	else if ((num % 5) == 0)
-
 	{
 
-	if (num != 100)
-
-	{
-
-	printf("Buzz ");
-
+		if (num != 100)
+		{
+		printf("Buzz ");
 	}
-
 	else
-
 	{
-
-	printf("Buzz");
-
+		printf("Buzz");
 	}
-
 	}
-
 	else
-
 	{
-
-	printf("%d ", num);
-
+		printf("%d ", num);
 	}
 
 	}
