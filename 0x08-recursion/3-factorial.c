93d9d9b7bdaaa8e12b8c3a6b033a569f9e3161cc
kingdaveo22 <kingdaveo22@gmail.com>
2022-09-28 08:01:37 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* factorial - Entry point
+* Desc: Entry
+*@n: int value
+* Return: Always 0 (Success)
+*/
+
+int factorial(int n)
+
+{
+
+	if (n >= 1)
+
+	{
+
+		return (n * factorial(n - 1));
+
+	}
+
+	if (n < 0)
+
+	{
+
+		return (-1);
+
+	}
+
+	return (1);
+
+}
