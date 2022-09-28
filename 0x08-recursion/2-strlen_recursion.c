f2c30f376bdf10bf6f1f667335baad849ae53420
kingdaveo22 <kingdaveo22@gmail.com>
2022-09-28 07:59:02 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - Entry point
+* Desc: Entry
+*@s: pointer
+* Return: Always 0 (Success)
+*/
+
+int _strlen_recursion(char *s)
+
+{
+	if (*s != '\0')
+
+	{
+
+		return (1 + _strlen_recursion(s + 1));
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+}
