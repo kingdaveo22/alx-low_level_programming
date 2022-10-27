3293921bbb2ad66dd5be539cc0e8c3f29544c876
kingdaveo22 <kingdaveo22@gmail.com>
2022-10-27 05:38:13 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,27 +2,26 @@
 
 /**
 * free_listint - frees a linked list
-* @head: listint_t list to be freed
+* @head: head of the list
+*
+* Return: void
 */
 
 void free_listint(listint_t *head)
 
 {
+	listint_t *temp;
 
-    listint_t *temp;
+	while (head != NULL)
 
+	{
 
+		temp = head;
 
-    while (head)
+		head = head->next;
 
-    {
+		free(temp);
 
-        temp = head->next;
-
-        free(head);
-
-        head = temp;
-
-    }
+	}
 
 }
